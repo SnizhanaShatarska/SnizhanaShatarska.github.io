efda1df0f08a4fa2ae40c700a576c190832197da
SnizhanaShatarska <78352887+SnizhanaShatarska@users.noreply.github.com>
2025-01-05 22:30:18 +0200

    Add files via upload

--- Project/script/script.js
+++ Project/script/script.js
@@ -1,10 +1,11 @@
-const burgerIcons = document.getElementById('burgerIcons')
-const openIcon = document.getElementById('openIcon')
-const closeIcon = document.getElementById('closeIcon')
-const menuList = document.getElementById('menuList')
+const burgerIcons = document.getElementById('burgerIcons');
+const openIcon = document.getElementById('openIcon');
+const closeIcon = document.getElementById('closeIcon');
+const menuList = document.getElementById('menuList');
 
 burgerIcons.addEventListener('click', () => {
   menuList.classList.toggle('show-menu')
   openIcon.classList.toggle('hide')
   closeIcon.classList.toggle('hide')
-})
+});
+
