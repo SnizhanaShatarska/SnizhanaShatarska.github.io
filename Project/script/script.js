dbe4ac1686c06cb9e26016b08a1457c947604bf4
SnizhanaShatarska <78352887+SnizhanaShatarska@users.noreply.github.com>
2025-01-19 22:43:17 +0200

    Add files via upload

--- Project/script/script.js
+++ Project/script/script.js
@@ -95,4 +95,77 @@ btnScrollTop.onclick = function() {
   });
 };
 
+// add reviews
+const url = "https://snizhanashatarska.github.io/reviews.json"
+
+function populate() {
+ axios.get(url)
+  .then(responce => {
+   populateReviews(responce.data.reviews);
+  })
+}
+
+function populateReviews(reviews) {
+ const swiper = document.getElementById("reviews");
+
+  for (var i = 0; i < reviews.length; i++) {
+    const swiperSlide = document.createElement('div')
+    swiperSlide.classList.add('swiper-slide');
+
+    const clientItem = document.createElement('div');
+    clientItem.classList.add('section-slider-item', 'client-item');
+    const iconsStar = document.createElement('div');
+    iconsStar.classList.add('icons-star');
+
+    for (let j = 0; j < reviews[i].rating; j++) {
+      const starIcon = document.createElement('i');
+      starIcon.classList.add('fa-solid', 'fa-star');
+      iconsStar.appendChild(starIcon);
+    }
+
+    const clientItemDesc = document.createElement('div');
+    clientItemDesc.classList.add('client-item-desc');
+
+    const clientitemTitle = document.createElement('div');
+    clientitemTitle.classList.add('section-slider-item-title');
+    clientitemTitle.textContent = reviews[i].review_title;
+
+    const clientText = document.createElement('p');
+    clientText.classList.add('section-text');
+    clientText.textContent = reviews[i].review;
+
+    clientItemDesc.appendChild(clientitemTitle);
+    clientItemDesc.appendChild(clientText);
+
+    const clientItemInfo = document.createElement('div');
+    clientItemInfo.classList.add('client-item-info');
+
+    const clientItemInfoImg = document.createElement('div');
+    clientItemInfoImg.classList.add('client-item-info-img');
+    const clientImg = document.createElement('img');
+    clientImg.src = reviews[i].avatar_url;
+    clientImg.alt = reviews[i].avatar_alt;
+    clientItemInfoImg.appendChild(clientImg);
+
+    const clientItemInfoName = document.createElement('div');
+    clientItemInfoName.classList.add('client-item-info-name');
+    clientItemInfoName.textContent = reviews[i].user_name;
+
+    const clientItemInfoCity = document.createElement('div');
+    clientItemInfoCity.classList.add('client-item-info-city');
+    clientItemInfoCity.textContent = reviews[i].user_city;
+
+    clientItemInfoName.appendChild(clientItemInfoCity);
+    clientItemInfo.appendChild(clientItemInfoImg);
+    clientItemInfo.appendChild(clientItemInfoName);
+
+    clientItem.appendChild(iconsStar);
+    clientItem.appendChild(clientItemDesc);
+    clientItem.appendChild(clientItemInfo);
+
+    swiperSlide.appendChild(clientItem);
+    swiper.appendChild(swiperSlide);
+  }
+}
+  populate();
 
